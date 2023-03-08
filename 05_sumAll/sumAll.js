8ed0b794b2482492a725a4dd6e4b5926cd797765
George Sotiropoulos <george@gsotworks.com>
2023-03-07 20:15:08 -0600

    Complete exercise 5
    Finished a number adder that checks primitive types and uses one-line
    code blocks.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,14 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    if (typeof n1 !== "number" || typeof n2 !== "number") return 'ERROR';
+    if (n1 < 0 || n2 < 0) return 'ERROR';
+
+    let sum = 0;
+
+    if (n2 < n1) [n1, n2] = [n2, n1]; // destructuring syntax to swap values
+
+    for (let i = n1; i <= n2; i++) sum += i;
+
+    return sum;
 
 };
 
