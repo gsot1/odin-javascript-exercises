e46399840cd9c555193f01bc64b5ee634167f458
George Sotiropoulos <george@gsotworks.com>
2023-03-07 20:20:25 -0600

    Complete exercise 6
    Created a leap year checker with ternary operators and a single if-else
    statement (excluding the type checker.)

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (typeof year !== "number") return 'ERROR';
 
+    if (year % 100 === 0) {
+        return year % 400 === 0 ? true : false;
+    } else {
+        return year % 4 === 0 ? true : false;
+    }
 };
 
 // Do not edit below this line
