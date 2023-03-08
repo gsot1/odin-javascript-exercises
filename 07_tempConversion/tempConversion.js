22f05c9526a95f7c8a35692b85d122dba63602eb
George Sotiropoulos <george@gsotworks.com>
2023-03-07 20:26:48 -0600

    Complete exercise 7
    Wrote a temperature conversion function pair with Math.EPSILON for
    precision.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  let decimal = (fah - 32) / 1.8;
+  return Math.round((decimal + Number.EPSILON) * 10) / 10; // EPSILON ensures <1-bit accuracy
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let decimal = cel * 1.8 + 32;
+  return Math.round((decimal + Number.EPSILON) * 10) / 10;
 };
 
 // Do not edit below this line
