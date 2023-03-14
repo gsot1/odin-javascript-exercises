e3fadbc860fec8e0cfe1a204d8c0f95ee12bd06b
George Sotiropoulos <george@gsotworks.com>
2023-03-14 02:11:59 -0500

    Complete exercise 12
    Wrote a .reduce() function that chooses  an object from an array based
    on the widest gap in two of its keyed values.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, curr) => {
+        let prevAge;
+        let currAge;
 
+        if (!oldest.yearOfDeath) {
+            prevAge = new Date().getFullYear() - oldest.yearOfBirth;
+        } else {
+            prevAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        }
+
+        if (!curr.yearOfDeath) {
+            currAge = new Date().getFullYear() - curr.yearOfBirth;
+        } else {
+            currAge = curr.yearOfDeath - curr.yearOfBirth;
+        }
+
+        return prevAge < currAge ? curr : oldest;
+
+    });
 };
 
 // Do not edit below this line
