d23cb62a23da3480c71d9555a3b6e73a5088fc56
George Sotiropoulos <george@gsotworks.com>
2023-03-14 01:43:53 -0500

    Complete exercise 10
    Wrote a fibonacci sequence generator using a fixed-length forloop.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    x = Number(x);
+    if (x === NaN || x < 1) return 'OOPS';
+    
+    let seq = [0, 1];
 
+    for (let i = 2; i <= x; i++) {
+        seq[i] = seq[i - 2] + seq[i - 1];
+    }
+
+    return seq[seq.length-1];
 };
 
 // Do not edit below this line
