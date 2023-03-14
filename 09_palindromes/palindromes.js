049cf99bd87ff44f574d745df31cc7e532d5a566
George Sotiropoulos <george@gsotworks.com>
2023-03-14 01:29:07 -0500

    Complete exercise 9
    Wrote a palindrome checker by comparing two halves, but could have
    compared entire reverse copies of the string.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    
+    let half1, half2;
+    str = str.replace(/\W+/g, "").toLowerCase();
 
+    if (str.length % 2 === 1) { // check for a middle letter to ignore
+        half1 = str.slice(0, str.length/2);
+        half2 = str.slice(str.length/2 + 1);
+    } else {
+        half1 = str.slice(0, str.length/2);
+        half2 = str.slice(str.length/2);
+    }
+
+    return half1 === half2.split("").reverse().join("");
 };
 
 // Do not edit below this line
